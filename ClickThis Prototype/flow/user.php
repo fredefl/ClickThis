7bad1624d197d9ee390e761b286763eb08428fd1
Bo Thomsen <boh1996@gmail.com>
2011-12-29 16:26:56 +0100

    Updated the profile in flow and prototype ready to merge flow and prototype.

--- ClickThis Prototype/flow/user.php
+++ ClickThis Prototype/flow/user.php
@@ -1,3 +1,20 @@
+<?php
+$names = array('32' => 'Car Magazine','3' => 'Bo Thomsen');
+if(isset($_GET['user_id'])){
+	$user = $_GET['user_id'];
+	$userid = 'user_'.$user;
+	if(isset($names[$user])){
+		$name = $names[$user];
+	}
+	else{
+		$name = $user;	
+	}
+}
+else{
+	$name = 'User not found';
+	$userid = 'notfound';	
+}
+?>
 <!DOCTYPE HTML>
 <html>
 <head>
@@ -9,19 +26,71 @@
 <link rel="stylesheet" type="text/css" href="../css/style.css"/>
 <link rel="apple-touch-icon" href="../images/ClickThis64.png" />
 <meta charset="utf-8">
-<title>User <?php echo $_GET['user_id']; ?></title>
+<title><?php echo $name; ?></title>
 </head>
 <body>
-	<div id="3">
-    
-    </div>
-	<div id="32">
-    
-    </div>
+	<input type="hidden" id="user" value="<?php echo $userid; ?>" />
+	<div id="jqt">
+        <div id="home" class="current">
+            <div class="toolbar">
+                <h1>ClickThis</h1>
+                <a class="button slideup" id="infoButton" href="#about">About</a>
+            </div>
+            <div id="notfound" class="Disabled">
+                <ul class="rounded" >
+                        <ul class="arrow">
+            				<li><h1 style="color:#FFFFFF;">User not found</h1></li>
+                        </ul>
+                </ul>            
+            </div>
+            <div id="user_3" class="Disabled">
+                <div id="Avatar">
+                    <a title="Bo Thomsen" href="http://illution.dk/ClickThisPrototype/profile.html#2"><img src="http://www.gravatar.com/avatar/dc07576afa6b5b172a378d6f5eb05f5f?s=256" title="Bo Thomsen" alt="Bo Thomsen" /></a>
+                </div>
+                <div id="Profile">
+                    <label for="Name">Name</label><br>
+                    <input type="text" id="Name" value="Bo Thomsen" disabled /><br>
+                    <label for="Email">Email</label><br>
+                    <input type="text" id="Email" value="bo@illution.dk"  disabled/><br>
+                    <label for="Email">Location</label><br>
+                    <select id="Location" disabled>
+                        <optgroup title="Europe" label="Europe">Europe</optgroup>
+                            <option selected value="dk">Denmark</option>
+                            <option value="en">England</option>
+                            <option value="de">Germany</option>
+                            <option value="fr">France</option>
+                        <optgroup title="Asia" label="Asia">Asia</optgroup>
+                            <option value="cn">China</option>
+                    </select>
+                </div>    
+            </div>
+            <div id="user_32" class="Disabled">
+                <div id="Avatar">
+                    <a title="Car Magazine" href="#2"><img src="../images/Grey Bugatti Veyron.png" title="Car Magazine" alt="Car Magazine" /></a>
+                </div>
+                <div id="Profile">
+                    <label for="Name">Name</label><br>
+                    <input type="text" id="Name" value="Car Magazine"  disabled/><br>
+                    <label for="Email">Email</label><br>
+                    <input type="text" id="Email" value="info@carmagazine.co.uk" disabled/><br>
+                    <label for="Email">Location</label><br>
+                    <select id="Location" disabled>
+                        <optgroup title="Europe" label="Europe">Europe</optgroup>
+                            <option value="dk">Denmark</option>
+                            <option selected value="en">England</option>
+                            <option value="de">Germany</option>
+                            <option value="fr">France</option>
+                        <optgroup title="Asia" label="Asia">Asia</optgroup>
+                            <option value="cn">China</option>
+                    </select>
+                </div>
+            </div>
+    	</div>    
+    </div>    
 	<script src="../js/prefixfree.min.js"></script>
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"></script>
     <script src="http://cdn.illution.dk/JS/jquery.hashchange.min.js"></script>
     <script src="../js/script.js"></script>
-    <script type="text/javascript" src="script.js"></script>
+    <script type="text/javascript" src="../js/user.js"></script>
 </body>
 </html>
--- ClickThis Prototype/js/user.js
+++ ClickThis Prototype/js/user.js
@@ -1,15 +1,17 @@
 $(document).ready(function() {
+	var page = $('#user').val();
+	changePage(page);
 	/* Check for page */
-	$(window).hashchange();
+	//$(window).hashchange();
 });
 
-$(window).hashchange( function(){
+/*$(window).hashchange( function(){
 	var Hash = location.hash;
 	if(Hash != null && Hash != undefined && Hash != ''){
-		page = Hash.replace('#','');
+		user = Hash.replace('#','');
 		changePage(page);
 	}
-});
+});*/
 
 /**
 * This function change the page content
@@ -20,7 +22,7 @@ function changePage(Page){
 	var NewPage = $('#'+Page);
 	var oldPage = $('#'+$('#currentpage').val());
 	if(Page != null && Page != undefined){
-		oldPage.removeClass('Active');
+		//oldPage.removeClass('Active');
 		oldPage.addClass('Disabled');
 		$('#currentpage').val(Page);
 		NewPage.removeClass('Disabled').addClass('Active');
