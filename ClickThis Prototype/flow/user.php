ecc97b9842f1f96428f973b245285f6496df0561
Bo Thomsen <boh1996@gmail.com>
2011-12-29 15:55:04 +0100

    Updated some documentation and user profile is WIP
    Signed-off-by: Bo Thomsen <boh1996@gmail.com>

--- ClickThis Prototype/flow/user.php
+++ ClickThis Prototype/flow/user.php
@@ -1,10 +1,27 @@
 <!DOCTYPE HTML>
 <html>
 <head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0;">
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="apple-mobile-web-app-status-bar-style" content="black">
+<link rel="stylesheet" type="text/css" href="../css/jqtouch.css"/>
+<link rel="stylesheet" type="text/css" href="../css/jqt/theme.css"/>
+<link rel="stylesheet" type="text/css" href="../css/style.css"/>
+<link rel="apple-touch-icon" href="../images/ClickThis64.png" />
 <meta charset="utf-8">
 <title>User <?php echo $_GET['user_id']; ?></title>
 </head>
-
 <body>
+	<div id="3">
+    
+    </div>
+	<div id="32">
+    
+    </div>
+	<script src="../js/prefixfree.min.js"></script>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"></script>
+    <script src="http://cdn.illution.dk/JS/jquery.hashchange.min.js"></script>
+    <script src="../js/script.js"></script>
+    <script type="text/javascript" src="script.js"></script>
 </body>
 </html>
