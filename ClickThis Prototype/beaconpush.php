c940b97f0b5bcee364390ddd53af627bd916e698
fredefl <frederiklassen@gmail.com>
2012-01-01 14:29:30 +0100

    Commented beaconpush.php.

--- ClickThis Prototype/beaconpush.php
+++ ClickThis Prototype/beaconpush.php
@@ -1,20 +1,21 @@
 <?php
-//set POST variables
+// Set set the URL
 $url = 'http://api.beaconpush.com/1.0.0/ed02c2f4/channels/mychannel';
 
-//open connection
+// Open the connection
 $ch = curl_init();
 
-//set the url, number of POST vars, POST data
+// Set the url, the number of POST variables and the POST data.
 curl_setopt($ch,CURLOPT_URL,$url);
 curl_setopt($ch,CURLOPT_HTTPHEADER,array('X-Beacon-Secret-Key: e3ebeaad362d0d4887c449dfe232fc29c5d614cb'));
 curl_setopt($ch,CURLOPT_POST,1);
 curl_setopt($ch,CURLOPT_POSTFIELDS,'{"update":""}');
 
-//execute post
+// Execute POST request.
 $result = curl_exec($ch);
+// Echo the result.
 echo $result;
 
-//close connection
+// Close the connection.
 curl_close($ch);
 ?>
