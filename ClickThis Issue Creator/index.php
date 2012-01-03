4c753c10ae81fa069c020292b1257aac5a8233ff
fredefl <frederiklassen@gmail.com>
2012-01-03 20:40:04 +0100

    Added basic html to ClickThis Issue Creator.

--- ClickThis Issue Creator/index.php
+++ ClickThis Issue Creator/index.php
@@ -1,11 +1,20 @@
 <?php
-require_once 'Github/Autoloader.php';
-Github_Autoloader::register();
-$Github = new Github_Client();
-$Github->authenticate("fredefl","3f7ed211500b2b50749d34cf2a9fc32b");
-$Result = $Github->getIssueApi()->open('fredefl', 'ClickThis', 'The issue title', 'The issue body');
-$Id = $Result['number'];
-$Github->getIssueApi()->addLabel('fredefl', 'ClickThis','User',$Id);
+if($_POST['title']){
+	require_once 'Github/Autoloader.php';
+	Github_Autoloader::register();
+	$Github = new Github_Client();
+	$Github->authenticate("fredefl","3f7ed211500b2b50749d34cf2a9fc32b");
+	$Result = $Github->getIssueApi()->open('fredefl', 'ClickThis', $_POST['title'], $_POST['description']);
+	$Id = $Result['number'];
+	$Github->getIssueApi()->addLabel('fredefl', 'ClickThis','User',$Id);
 ?>
 <h1>Thanks for your feedback</h1>
 <a href="http://illution.dk/ClickThisPrototype">Click here to go back</a>
+<?php
+} else {
+?>
+<h1>You did not write anything...</h1>
+<a href="http://illution.dk/ClickThisPrototype">Click here to go back</a>
+<?php
+}
+?>
