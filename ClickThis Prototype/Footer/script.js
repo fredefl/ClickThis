4f58c48b3534721f8cdf0b6eea94b5d4f515a551
Bo Thomsen <boh1996@gmail.com>
2012-01-03 16:49:58 +0100

    Updated my width algorithm

--- ClickThis Prototype/Footer/script.js
+++ ClickThis Prototype/Footer/script.js
@@ -25,19 +25,37 @@ $(document).ready(function() {
 	center();
 });
 
+function WindowWidth(){
+	var width = 1460;
+	if (document.body && document.body.offsetWidth) {
+	 width = document.body.offsetWidth;
+	}
+	if (document.compatMode=='CSS1Compat' &&
+		document.documentElement &&
+		document.documentElement.offsetWidth ) {
+	 	width = document.documentElement.offsetWidth;
+	}
+	if (window.innerWidth && window.innerHeight) {
+	 	width = window.innerWidth;
+	}
+	return width;	
+}
+
 function center(){
-	var left = 70;
-	var right = 70;
-	var elementWidth = 52;
+	var marginLeft = 75;
+	var marginRight = 75;
 	var elementMarginRight = 20;
 	var elementMarginLeft = 0;
 	var numberOfElements = 0;
-	var width = window.width-left-right;
+	var elementWidth = 52+elementMarginRight+elementMarginLeft;
+	var width = window.width-marginLeft-marginRight;
 	$('#progress-container').css('width',width);
 	var innerWidth = 0;
 	$('.progress').each(function(index, element) {
        innerWidth += $(this).width()+22;
     });
 	$('#progress-inner-container').css('width',innerWidth);
-	numberOfElements = width/((elementWidth+elementMarginRight+elementMarginLeft)-elementMarginRight);
+	numberOfElements = (WindowWidth()-marginLeft-marginRight)/((elementWidth)-elementMarginRight);
+	console.log('Number of Elements:'+Math.round(numberOfElements));
+	console.log('Width:'+Math.round((parseInt(WindowWidth())-parseInt(marginLeft)-parseInt(marginRight))/(Math.round(elementWidth))));
 }
