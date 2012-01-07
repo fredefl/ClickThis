005d42e84d2dd0ff684984e78246983cf1f20e62
Frederik Lassen <frederiklassen@gmail.com>
2012-01-07 01:00:16 +0100

    Added Android Project. (With APK :D)

---
+++ ClickThis Android App/src/dk/illution/clickthis/MainActivity.java
@@ -0,0 +1,65 @@
+package dk.illution.clickthis;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+	WebView mainWebView;
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+    	// The standard crap
+    	super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        setUpWebView();
+    }
+    
+    public void setUpWebView () {
+    	// Find the WebView element
+        mainWebView = (WebView) findViewById(R.id.mainWebView);
+        
+        // Enable JavaScript and DOM storage (for example localStorage)
+        mainWebView.getSettings()
+        	.setJavaScriptEnabled(true);
+        mainWebView.getSettings()
+        	.setDomStorageEnabled(true);
+        
+        // Add the JavaScript interface
+        mainWebView.addJavascriptInterface(new JavaScriptInterface(this), "ClickThisApp");
+        
+        // Handle redirects so it won't open in the built in browser
+        mainWebView.setWebViewClient(new WebViewClient() {
+            public boolean shouldOverrideUrlLoading(WebView view, String url){
+                // do your handling codes here, which url is the requested url
+                // probably you need to open that url rather than redirect:
+                view.loadUrl(url);
+                return false; // then it is not handled by default action
+           }
+        });
+        // Clear cache
+        mainWebView.clearCache(true);
+        // Load the ClickThis Prototype
+        mainWebView.loadUrl("http://illution.dk/ClickThisPrototype/home.html");
+    }
+    
+    // The JavaScript interface
+    public class JavaScriptInterface {
+        Context mContext;
+
+        /** Instantiate the interface and set the context */
+        JavaScriptInterface(Context c) {
+            mContext = c;
+        }
+        
+        // Terminates the native android app, FROM JAVASCRIPT!!!
+        public void terminateApp () {
+        	//MainActivity.this.moveTaskToBack(true);
+        	Toast.makeText(mContext, "hehe", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+}
