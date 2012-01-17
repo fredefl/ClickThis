34aa40229462b0c8c25cd42592ac3fde0691a652
Bo Thomsen <boh1996@gmail.com>
2012-01-17 16:27:35 +0100

    Added the provider class and updated the link to it in script.js
    Signed-off-by: Bo Thomsen <boh1996@gmail.com>

--- ClickThis Prototype/test/script.js
+++ ClickThis Prototype/test/script.js
@@ -9,7 +9,7 @@ var numberPerPage = 6; // This variable set how many providers there will be sho
 var numberPerRow = 3; //This variable sets how many providers there will be shown per row
 var oldPage; //This variable stores the last disabled page
 var oldPageChangeType; //This variable is used in the edit box the re-create what it changed
-var changeing = false;
+var changeing = false; //This variable stores the state of the page, true means that the page is changing
 
 $('#edit').click(function() {
 	var editBox = $('#edit-box');
@@ -45,6 +45,8 @@ $('#right').click(function() {
 		}
 		else{
 			changeing = true;
+			newPage = $(pageChangeType+':first');
+			animate(newPage,thisPage);
 		}
 	}
 });
@@ -86,6 +88,8 @@ $('#left').click(function () {
 		}
 		else{
 			changeing = true;
+			newPage = $(pageChangeType+':last');
+			animate(newPage,thisPage);
 		}
 	}
 });
@@ -104,16 +108,16 @@ function showUserProviders() {
 	}
 	var currentIndex = 0;
 	for(var i = 1;i <= numberOfPages;i++) {
-		var page = addPage($("#box"),"user",i,"Default");
-		var container = addContainer(page);
-		var row1 = addRow(container);
-		var row2 = addRow(container);
+		var page = provider.addPage($("#box"),"user",i,"Default");
+		var container = provider.addContainer(page);
+		var row1 = provider.addRow(container);
+		var row2 = provider.addRow(container);
 		for(var number = 0;number <= numberPerPage-1;number++) {
 			if (currentIndex < userProviders.length) {
 				if (number < numberPerRow) {
-					addProvider(providers[userProviders[currentIndex]],addColumn(row1));
+					provider.addProvider(providers[userProviders[currentIndex]],provider.addColumn(row1));
 				} else if (number < numberPerPage) {
-					addProvider(providers[userProviders[currentIndex]],addColumn(row2));				
+					provider.addProvider(providers[userProviders[currentIndex]],provider.addColumn(row2));				
 				}
 				currentIndex++;
 			}
@@ -190,191 +194,58 @@ $(window).hashchange( function () {
 })
 
 /**
-* This function creates a table, this function will be used to create the page tables
-* @param object obj This parameter is the jquery object of the outer div or outer container
-* @param object cellspacing This is an optional parameter, to set the table cellspacing if its not set then the value will be 10
-* @returns object This function returns the jquery object of the container
-*/
-function addContainer(obj,cellspacing) {
-	var container = $("<table></table>");
-	if (typeof cellspacing != "number") {
-		cellspacing = 10;
-	}
-	container.attr("cellspacing",cellspacing);
-	if (typeof obj == "object" && obj != null) {
-		obj.append(container);
-	}
-	return container;
-}
-
-/**
 * This function is called by the succes callback of the providers ajax request.
 * @param function An optinal callback function when ready
 */
 function start(callback) {
 
 		//Page 1
-	var page1 = addPage($("#box"),"default","1","Disa"),
-		page1Container = addContainer(page1),
-		page1Row1 = addRow(page1Container),
-		page1Row2 = addRow(page1Container),
+	var page1 = provider.addPage($("#box"),"default","1","Disa"),
+		page1Container = provider.addContainer(page1),
+		page1Row1 = provider.addRow(page1Container),
+		page1Row2 = provider.addRow(page1Container),
 		//Page 2
-		page2 = addPage($("#box"),"default","2","Disabled"),
-		page2Container = addContainer(page2),
-		page2Row1 = addRow(page2Container),
-		page2Row2 = addRow(page2Container),
+		page2 = provider.addPage($("#box"),"default","2","Disabled"),
+		page2Container = provider.addContainer(page2),
+		page2Row1 = provider.addRow(page2Container),
+		page2Row2 = provider.addRow(page2Container),
 		//Page 3
-		page3 = addPage($("#box"),"default","3","Disabled"),
-		page3Container = addContainer(page3),
-		page3Row1 = addRow(page3Container),
-		page3Row2 = addRow(page3Container);
+		page3 = provider.addPage($("#box"),"default","3","Disabled"),
+		page3Container = provider.addContainer(page3),
+		page3Row1 = provider.addRow(page3Container),
+		page3Row2 = provider.addRow(page3Container);
 	
 	//Page One Row 1
-	addProvider(providers.Google,addColumn(page1Row1));
-	addProvider(providers.ClickThis,addColumn(page1Row1));
-	addProvider(providers.MySpace,addColumn(page1Row1));
+	provider.addProvider(providers.Google,provider.addColumn(page1Row1));
+	provider.addProvider(providers.ClickThis,provider.addColumn(page1Row1));
+	provider.addProvider(providers.MySpace,provider.addColumn(page1Row1));
 	
 	//Page One Row 2
-	addProvider(providers.Facebook,addColumn(page1Row2));
-	addProvider(providers.Twitter,addColumn(page1Row2));
-	addProvider(providers.LinkedIn,addColumn(page1Row2));
+	provider.addProvider(providers.Facebook,provider.addColumn(page1Row2));
+	provider.addProvider(providers.Twitter,provider.addColumn(page1Row2));
+	provider.addProvider(providers.LinkedIn,provider.addColumn(page1Row2));
 	
 	//Page Two Row 1
-	addProvider(providers.OpenId,addColumn(page2Row1));
-	addProvider(providers.GitHub,addColumn(page2Row1));
-	addProvider(providers.Vimeo,addColumn(page2Row1));
+	provider.addProvider(providers.OpenId,provider.addColumn(page2Row1));
+	provider.addProvider(providers.GitHub,provider.addColumn(page2Row1));
+	provider.addProvider(providers.Vimeo,provider.addColumn(page2Row1));
 	
 	//Page Two Row 2
-	addProvider(providers.StumbleUpon,addColumn(page2Row2));
-	addProvider(providers.Youtube,addColumn(page2Row2));
-	addProvider(providers.Tumblr,addColumn(page2Row2));
+	provider.addProvider(providers.StumbleUpon,provider.addColumn(page2Row2));
+	provider.addProvider(providers.Youtube,provider.addColumn(page2Row2));
+	provider.addProvider(providers.Tumblr,provider.addColumn(page2Row2));
 	
 	//Page Three Row 1
-	addProvider(providers.GooglePlus,addColumn(page3Row1));
-	addProvider(providers.FriendFeed,addColumn(page3Row1));
-	addProvider(providers.Flickr,addColumn(page3Row1));
+	provider.addProvider(providers.GooglePlus,provider.addColumn(page3Row1));
+	provider.addProvider(providers.FriendFeed,provider.addColumn(page3Row1));
+	provider.addProvider(providers.Flickr,provider.addColumn(page3Row1));
 	
 	//Page Three Row 2
-	addProvider(providers.Blogger,addColumn(page3Row2));
+	provider.addProvider(providers.Blogger,provider.addColumn(page3Row2));
 		
 	showUserProviders();
 	if (typeof callback == "function") {
 		callback();
-	}
-}
-
-/**
-* This function creates a new row in a table with the tr tags,
-* and returns the jquery object if a table is deffined as an object in obj the row is appended to it.
-* @param object The jquery object of the table
-* @returns object The jquery object of the new row
-*/
-function addRow(obj) {
-	var row = $("<tr></tr>");
-	if (typeof obj == "object" && obj != null) {
-		obj.append(row);
-	}
-	return row;
-}
-
-/**
-* This function adds a page to the container with the specified parameters
-* @param object obj The container you wish to add the page div to
-* @param string type This is the type of the page, "user" or "default"/page
-* @param string name The name of the page div the final id will be the choosen keyoed user or page + name
-* @param string state The state of the page "Disabled" or "Active"
-* @retunrs object This function returns a jquery object
-*/
-function addPage(obj,type,name,state) {
-	var div = $("<div></div>");
-	var objectName;
-	if (type === "default") {
-		div.addClass('page');
-		div.addClass('default');
-		div.attr("id",pageKeyword+name);
-		objectName = pageKeyword+name;
-	}
-	else if (type === "user") {
-		div.addClass('page');
-		div.addClass('user');
-		div.attr("id",userPageKeyword+name);
-		objectName = userPageKeyword+name;
-	}
-	if (state === "Active") {
-		div.addClass("Active");	
-	} else {
-		div.addClass("Disabled");	
-	}
-	if (typeof obj == "object" && obj != null) {
-		obj.append(div);
-	}
-	return $('#'+objectName);
-}
-
-/**
-* This function adds a column to a table and if obj is specified the column will be appended too.
-* @param obj The row/tr tag you wish to add the td/column too
-* @returns object The jquery object of the newly created column
-*/
-function addColumn(obj) {
-	var column = $("<td></td>");
-	if (typeof obj == "object" && obj != null) {
-		obj.append(column);
-	}
-	return column;
-}
-
-/**
-* This function adds the necesary html code for provider element,
-* and if obj is specified the element will also be append to to obj
-* @param object obj The jquery object you wish to add the provider to
-*
-* @param array data This parameter is optional it's used to specify other rows then standard,
-* but beaware that the html tag and the class variable must be the same.
-* @returns object The created jquery object of the provider
-*/
-function addProvider(provider,obj,data) {
-	if (typeof provider == "object") {
-		var linkTag = $("<a></a>"); //Makes the a tag
-		var content = $("<img></img>"); //Makes the content img tag
-		
-		//Adds the href to the a tag if its set
-		if (provider.Link != undefined && provider.Link != null) {
-			linkTag.attr("href",provider.Link);
-		}
-		
-		//Adds the image src if its set
-		if (provider.Image != undefined && provider.Image != null) {
-			content.attr("src",provider.Image);
-		}
-		
-		//Adds the image alt if its set
-		if (provider.Alt != undefined && provider.Alt != null) {
-			content.attr("alt",provider.Alt);
-		}
-		
-		//Adds the image title if its set
-		if (provider.Title != undefined && provider.Title != null) {
-			content.attr("title",provider.Title);
-		}
-		
-		//If there is extra attributes to be set, defined in data then set em
-		if (typeof data == "object" && data != null) {
-			$(data).each(function (index, element) {
-                content.attr(element,provider[element])
-            });
-		}
-		
-		content.addClass('provider');
-		linkTag.addClass('providerLink');
-		//Append the img tag to the a tag
-		linkTag.append(content);
-		
-		//If an append obj is set append the provider to it
-		if (typeof obj == "object" && obj != null) {
-			obj.append(linkTag);
-		}
-		return linkTag;
 	}
 }
 
