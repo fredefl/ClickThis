335546990dc643cc54d758954c2c78db64ae7316
Frederik Lassen <frederiklassen@gmail.com>
2012-01-31 22:14:28 +0100

    Converted image paths to the new provider directory.

--- ClickThis Prototype/test/providers.php
+++ ClickThis Prototype/test/providers.php
@@ -3,22 +3,22 @@
 * Properties: Image,Alt,Title,Link
 */
 $Providers = array(
-	"Google" => array("Image" => "images/Google.png"),
-	"ClickThis" => array("Image" => "images/ClickThis.png"),
-	"MySpace" => array("Image" => "images/MySpace.png"),
-	"Facebook" => array("Image" => "images/Facebook.png"),
-	"Twitter" => array("Image" => "images/Twitter.png"),
-	"LinkedIn" => array("Image" => "images/LinkedIn.png"),
-	"Blogger" => array("Image" => "images/Blogger.png"),
-	"GitHub" => array("Image" => "images/github.png"),
-	"OpenId" => array("Image" => "images/Openid.png"),
-	"StumbleUpon" => array("Image" => "images/StumbleUpon.png"),
-	"Vimeo" => array("Image" => "images/Vimeo.png"),
-	"Youtube" => array("Image" => "images/Youtube.png"),
-	"Tumblr" => array("Image" => "images/Tumblr.png"),
-	"GooglePlus" => array("Image" => "images/GooglePlus.png","Alt" => "Google+","Title" => "Google+"),
-	"FriendFeed" => array("Image" => "images/FriendFeed.png","Alt" => "friendfeed","Title" => "friendfeed"),
-	"Flickr" => array("Image" => "images/Flickr.png")
+	"Google" => array("Image" => "images/providers/128/Google.png"),
+	"ClickThis" => array("Image" => "images/providers/128/ClickThis.png"),
+	"MySpace" => array("Image" => "images/providers/128/MySpace.png"),
+	"Facebook" => array("Image" => "images/providers/128/Facebook.png"),
+	"Twitter" => array("Image" => "images/providers/128/Twitter.png"),
+	"LinkedIn" => array("Image" => "images/providers/128/LinkedIn.png"),
+	"Blogger" => array("Image" => "images/providers/128/Blogger.png"),
+	"GitHub" => array("Image" => "images/providers/128/github.png"),
+	"OpenId" => array("Image" => "images/providers/128/Openid.png"),
+	"StumbleUpon" => array("Image" => "images/providers/128/StumbleUpon.png"),
+	"Vimeo" => array("Image" => "images/providers/128/Vimeo.png"),
+	"Youtube" => array("Image" => "images/providers/128/Youtube.png"),
+	"Tumblr" => array("Image" => "images/providers/128/Tumblr.png"),
+	"GooglePlus" => array("Image" => "images/providers/128/GooglePlus.png","Alt" => "Google+","Title" => "Google+"),
+	"FriendFeed" => array("Image" => "images/providers/128/FriendFeed.png","Alt" => "friendfeed","Title" => "friendfeed"),
+	"Flickr" => array("Image" => "images/providers/128/Flickr.png")
 );
 foreach($Providers as $Name => $Provider){
 	if(!isset($Provider["Title"])){
