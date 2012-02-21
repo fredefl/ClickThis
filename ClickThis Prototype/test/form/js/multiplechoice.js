89a4d337fb35158e89b62e80af7b8903c66da20e
Bo Thomsen <boh1996@gmail.com>
2012-02-21 16:56:32 +0100

    Made a form test
    Signed-off-by: Bo Thomsen <boh1996@gmail.com>

---
+++ ClickThis Prototype/test/form/js/multiplechoice.js
@@ -0,0 +1,26 @@
+"use strict";
+/*
+* This event happens when the document is ready to be used
+*/
+/*
+$(document).ready(function () {
+	var question1 = [];
+
+	addTitle('Which sport(s) do you practice?', $('#question_1'), 'Title');
+
+	question1[0] = ['multi', 'green', 'Athletics', 0];
+	question1[1] = ['multi', 'green', 'Badminton', 0];
+	question1[2] = ['multi', 'green', 'Football', 0];
+	question1[3] = ['multi', 'green', 'Handball', 0];
+	question1[4] = ['multi', 'green', 'Shooting', 0];
+	question1[5] = ['multi', 'green', 'Swimming', 0];
+	question1[6] = ['multi', 'green', 'Tennis', 0];
+	question1[7] = ['multi', 'green', 'Volleyball', 0];
+	question1[8] = ['multi', 'gold', 'Others', 0];
+	question1[9] = ['button', 'red', 'None', 0];
+	question1[10] = ['submit', 'orange', 'Send', 'sendButton1', 'http://illution.dk/ClickThisPrototype', null];
+	question(question1, 'question_1', true, true, '1', '#Title');
+});*/
+$(document).ready(function(){
+	$('.textfield').ata();
+});
---
+++ ClickThis Prototype/test/form/js/user.js
@@ -0,0 +1,22 @@
+"use strict";
+
+$(document).ready(function () {
+	var page = $('#user').val();
+	changePage(page);
+});
+
+/**
+* This function change the page content
+*
+* @param {string} page The id without the # of the page content container
+*/
+function changePage(Page) {
+	var NewPage = $('#' + Page),
+		oldPage = $('#' + $('#currentpage').val());
+	if (Page !== null && Page !== undefined) {
+		//oldPage.removeClass('Active');
+		oldPage.addClass('Disabled');
+		$('#currentpage').val(Page);
+		NewPage.removeClass('Disabled').addClass('Active');
+	}
+}
