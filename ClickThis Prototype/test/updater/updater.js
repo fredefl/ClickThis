fe5ca25b90edbaf484e516a8a023c1cf678b6df9
Frederik Lassen <frederiklassen@gmail.com>
2012-03-19 12:12:36 +0100

    Added empty updater....

---
+++ ClickThis Prototype/test/updater/updater.js
@@ -0,0 +1,23 @@
+var updater = {
+
+	update: function () {
+
+	},
+
+	updateCheck: function () {
+
+	},
+
+	calculateHashes: function () {
+
+	},
+
+	load: function () {
+
+	},
+
+	save: function () {
+		
+	}
+
+}
