088469d1f8454fa27f8bbe7836fabaf94b9dcfa7
Frederik Lassen <frederiklassen@gmail.com>
2012-03-21 21:59:38 +0100

    Commented updater.js line by line.

--- ClickThis Prototype/test/updater/updater.js
+++ ClickThis Prototype/test/updater/updater.js
@@ -24,16 +24,22 @@ var updater = {
 	 * @return {void}
 	 */
 	updateAll: function (data) {
+		// Declare variables
 		var item;
+		// Go though all update files
 		for (var i = 0; i <= data.items.length - 1; i++) {
+			// Get the item from the list
 			item = data.items[i];
+			// Save the name for use later
 			this.list[item.url] = item.name;
+			// Add it to the ajaxQueue
 			ajaxQueue.add({
 				url: item.url,
 				data: "",
 				group: "updateItems"
 			});
 		};
+		// Ignite the ajaxQueue
 		ajaxQueue.executeTasks();
 	},
 
@@ -42,9 +48,12 @@ var updater = {
 	 * @return {void}
 	 */
 	start: function () {
+		// Check if the updater already listens on ajaxQueue
 		if(!this.listens) {
+			// If it doesn't, listen
 			this.listen();
 		}
+		// Start downloading the update list (ASYNC)
 		this.downloadList();
 	},
 
@@ -54,24 +63,30 @@ var updater = {
 	 * @return {void}
 	 */
 	calculateHashes: function (data) {
+		// Declare variables
 		var item,
 			localData,
 			localMd5,
 			remoteMd5;
+		// Go though all update items on the list
 		for (var i = 0; i <= data.items.length - 1; i++) {
+			// Grab the item from the list
 			item = data.items[i];
+			// Save the name for later
 			this.list[item.url] = item.name;
+			// Get the remote md5 hash
 			remoteMd5 = item.md5;
+			// Get the data from localstorage
 			localData = localStorage[item.name] || "";
-			
+			// If the data is JSON, stringify it!
 			if(typeof(localData) == 'object') {
 				localData = JSON.stringify(localData);
 			}
+			// Calculate the md5 has for the local data
 			localMd5 = hex_md5(localData).toUpperCase();
-
-			console.log(remoteMd5, localData, localMd5);
-
+			// Chech for a (non)match
 			if(remoteMd5 !== localMd5) {
+				// If it doesn't match, go grab the data from the server (ASYNC)
 				ajaxQueue.add({
 					url: item.url,
 					data: "",
@@ -79,6 +94,7 @@ var updater = {
 				});
 			}
 		}
+		// Ignite the ajaxQueue
 		ajaxQueue.executeTasks();
 	},
 
@@ -87,21 +103,27 @@ var updater = {
 	 * @return {void}
 	 */
 	listen: function () {
+		// Register a callback on the updateList event
 		ajaxQueue.registerCallback({
 			type: "onSuccess",
 			group: "updateList"
 		}, function (data) {
+			// Calculate hashes if the update list has arrived
 			updater.calculateHashes(data.data);
 		});
+		// Register a callback on the updateItems event, that is executed when an items data has arrived
 		ajaxQueue.registerCallback({
 			type: "onSuccess",
 			group: "updateItems"
 		}, function (data) {
+			// If the data is JSON, stringify it!
 			if(typeof(data.data) == 'object') {
 				data.data = JSON.stringify(data.data);
 			}
+			// Get the name of the data, and save the data to localStorage
 			localStorage.setItem(updater.list[data.url], data.data);
 		});
+		// Report that the updater now listens
 		this.listens = true;
 	},
 
@@ -110,11 +132,13 @@ var updater = {
 	 * @return {void}
 	 */
 	downloadList: function () {
+		// Queue a download of the update list
 		ajaxQueue.add({
 			url: this.updateListUrl,
 			data: "",
 			group: "updateList"
 		});
+		// Fire up under the ajaxQueue
 		ajaxQueue.executeTasks();
 	},
 }
