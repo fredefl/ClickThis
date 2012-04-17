7ca1ca74dcd96809ef427e7b61bc061bac0d1463
Frederik Lassen <frederiklassen@gmail.com>
2012-04-17 21:32:36 +0200

    Removed crap.

--- ClickThis Prototype/js/clickthis.js
+++ ClickThis Prototype/js/clickthis.js
@@ -29,35 +29,6 @@ function isset(data) {
 	}
 }
 
-/**
-* This function change the page content
-*
-* @param {string} page The id without the # of the page content container
-*/
-function changePage(Page) {
-	var backButton = $('#backButton'),
-		NewPage = $('#' + Page),
-		oldPage = $('#' + $('#currentpage').val()),
-		url = new String(window.location);
-	if (Page !== null && Page !== undefined) {
-		oldPage.removeClass('Active');;
-		oldPage.addClass('Disabled');
-		$('#currentpage').val(Page);
-		NewPage.removeClass('Disabled').addClass('Active');
-	}
-	if (Page === 'user') {
-		backButton.addClass('Disabled');
-	} else {
-		backButton.removeClass('Disabled');
-	}
-	if (isOnPage('multiplechoice.html') || isOnPage('singlechoice.html') || isOnPage('buttons.html')) {
-		buttonResizer.resizeButtons(document.body);
-	}
-	if (isOnPage('home.html')) {
-		shortenTitle();
-	}
-	aboutText();
-}
 
 /**
 * This function change the page by disabling and enabling divs
@@ -68,83 +39,6 @@ function switchPage(backButton, NewPage, oldPage, Page) {
 		oldPage.addClass('Disabled');
 		$('#currentpage').val(Page);
 		NewPage.removeClass('Disabled').addClass('Active');
-	}
-}
-
-/**
-* This function show the about box
-*/
-function showAboutBox() {
-	var currentPage = $('#' + $('#currentpage').val()),
-		page = '#' + $('#currentpage').val(),
-		aboutBox = $('#aboutBox'),
-		backButton = $('#backButton');
-	backButton.attr('data-href', page);
-	backButton.attr('data-about', 'true');
-	aboutText();
-	backButton.removeClass('Disabled').addClass('Active');
-	currentPage.addClass('Disabled').removeClass('Active');
-	aboutBox.removeClass('Disabled').addClass('Active');
-	if (isOnPage('multiplechoice.html')) {
-		$('#questionsContainer').addClass('Disabled');
-	}
-}
-
-function startmode() {
-	var startDisabled,
-	back = $("#backButton");
-
-	if (isset(back.attr('data-start-mode'))) {
-		if (back.attr('data-start-mode') === 'active') {
-			startDisabled = false;
-		}
-		if (back.attr('data-start-mode') === 'disabled') {
-			startDisabled = true;
-		}
-	}
-	if (window.ClickThisApp) {
-		if (isset(back.attr('data-app-start-mode'))) {
-			if (back.attr('data-start-mode') === 'active') {
-				startDisabled = false;
-			}
-			if (back.attr('data-app-start-mode') === 'disabled') {
-				startDisabled = true;
-			}
-		}
-	} else {
-		if (isset(back.attr('data-page-start-mode'))) {
-			if (back.attr('data-page-start-mode') === 'active') {
-				startDisabled = false;
-			}
-			if (back.attr('data-page-start-mode') === 'disabled') {
-				startDisabled = true;
-			}
-		}
-	}
-	return startDisabled;
-}
-
-/**
-* This function hides the aboutbox
-*/
-function hideAboutBox() {
-	var aboutBox = $('#aboutBox'),
-		backButton = $('#backButton'),
-		currentPage = $('#' + $('#currentpage').val()),
-		page = '#' + $('#currentpage').val();
-	currentPage.removeClass('Disabled').addClass('Active');
-	backButton.attr('data-href', 'home.html');
-	backButton.removeAttr('data-about');
-	aboutText();
-	if (window.ClickThisApp && isOnPage("home.html")) {
-		backButton.removeClass('Disabled').addClass('Active');
-	}
-	if ($('#currentpage').val() == 'user') {
-		backButton.addClass('Disabled');
-	}
-	aboutBox.addClass('Disabled').removeClass('Active');
-	if (isOnPage('multiplechoice.html')) {
-		$('#questionsContainer').removeClass('Disabled');
 	}
 }
 
