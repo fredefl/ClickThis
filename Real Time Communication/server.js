bfaaa9f8cb9cb0f9a66050e8fcc2b3449b9d44d9
Frederik Lassen <frederiklassen@gmail.com>
2012-05-13 21:51:26 +0200

    Removed jSessionID again, is was causing a lot of trouble.

--- Real Time Communication/server.js
+++ Real Time Communication/server.js
@@ -14,16 +14,6 @@ var broadcast = {};
 
 // Create sockjs server
 var messageServer = sockjs.createServer({
-	jsessionid: function () {
-		var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
-		var string_length = 12;
-		var randomstring = '';
-		for (var i=0; i<string_length; i++) {
-			var rnum = Math.floor(Math.random() * chars.length);
-			randomstring += chars.substring(rnum,rnum+1);
-		}
-		return randomString;
-	}, 
 	log: function (severity, message) {
 		console.log(message);
 }});
